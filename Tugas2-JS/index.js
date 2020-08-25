70e862d0210bf2a05ffac8cd6276136aa48df8fc
Dea Niar <momentku15@gmail.com>
2020-08-25 18:51:29 +0700

    initial commit

---
+++ Tugas2-JS/index.js
@@ -0,0 +1,5 @@
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && false); // false
+console.log(false && true && true); // false
+console.log(true && true && true); // true 
